9396e3de6704ee5de4254dba6c1426ad88209148
Neluttu <neluttu@gmail.com>
2023-01-02 01:00:31 +0200

    Added .js comments

--- jQueryFAQAccordion.js
+++ jQueryFAQAccordion.js
@@ -1,10 +1,13 @@
+// jQuery script 
 $(document).ready(function() {
+    // When <h4> tag is clicked
     $(".question h4").click(function() {
-      if ($('.faqAnswer').is(':visible')) {
-          $(".faqAnswer").slideUp(300);
-          $('.faqContainer').removeClass("open");
-          $('h4').removeClass("open");
-      }
+        // Check to see if there is a toggled question and close it.
+        if ($('.faqAnswer').is(':visible')) {
+            $(".faqAnswer").slideUp(300);
+            $('.faqContainer').removeClass("open");
+            $('h4').removeClass("open");
+        }
 
       if ($(this).next(".faqAnswer").is(':visible')) {
           $(this).next(".faqAnswer").slideUp(300);
