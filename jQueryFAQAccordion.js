97011dd3342802c137d1c56bba45ad0d29f78b77
Neluttu <neluttu@gmail.com>
2023-01-02 01:03:10 +0200

    Finished adding .js comments

--- jQueryFAQAccordion.js
+++ jQueryFAQAccordion.js
@@ -9,15 +9,17 @@ $(document).ready(function() {
             $('h4').removeClass("open");
         }
 
-      if ($(this).next(".faqAnswer").is(':visible')) {
-          $(this).next(".faqAnswer").slideUp(300);
-          $(this).parent().removeClass("open");
-          $(this).removeClass("open");
-      } 
-      else { 
-          $(this).next(".faqAnswer").slideDown(300);
-          $(this).parent().addClass('open');
-          $(this).addClass('open');
-      }
+        if ($(this).next(".faqAnswer").is(':visible')) {
+            // If user clicks on an open question, closed it. Remove css classes.
+            $(this).next(".faqAnswer").slideUp(300);
+            $(this).parent().removeClass("open");
+            $(this).removeClass("open");
+        } 
+        else { 
+            // If user clicks on a question with an hidden answer, slideDown the answer and apply css classes.
+            $(this).next(".faqAnswer").slideDown(300);
+            $(this).parent().addClass('open');
+            $(this).addClass('open');
+        }
     });
   });
